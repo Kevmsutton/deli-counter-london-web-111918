cb4ea4e54a7daa66f5879118e50cc3299ff220ff
Kevin Sutton <kevmsutton@gmail.com>
2018-11-12 18:07:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(katz_deli)
+  new_array = []
+  counter = 0
+  if katz_deli.length > 0 
+    katz_deli.each do |person|
+    counter +=1
+    new_array.push(#{counter}. #{person}")
+end
+return new_array
+  else 
+    puts "The line is currently empty."
+end
+end
+
+line(["billy", "bobby", "Mikey"])
